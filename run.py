bf4fb235cbc3e6d8e006e17cf75a86700a63db8e
adriankiprono <tuimuradrian6@gmail.com>
2019-11-10 19:10:02 +0300

    add create function in run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
 #!usr/bin/env python3.8
-import pyperclip
+
 from user import User #import the user class
 from user import Credential #import the credential class
+def create_user(first_name,last_name,email,password,phone_number):
+    '''
+    function to create a new user
+    '''
+    new_user=User(first_name,last_name,email,password,phone_number)
+    return new_user
+
