75f45f7c9c33a7676253350f879dfabc5781a847
adriankiprono <tuimuradrian6@gmail.com>
2019-11-11 08:06:11 +0300

    add function to create and another to save

--- run.py
+++ run.py
@@ -12,5 +12,27 @@ def save_user(first_name,last_name,email,password,phone_number):
     '''
     function to save user
     '''
-    User.user_list.append()
+    user.save_user
+def delete_user(user):
+    '''
+    function to delete user
+    '''
+    user.delete_user
+def find_user(email,password):
+    '''
+    function to find user
+    '''
+    return User.find_by_email(email,password)
+def create_credential(user_name,password,account_name):
+    '''
+    function to create credential
+    '''
+    new_credential=Credential(user_name,password,account_name)
+    return new_credential
+def save_credential(credential):
+    '''
+    function to save credential
+    '''
+    credential.save_credential()
+
 
